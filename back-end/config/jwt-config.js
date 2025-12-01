32742b85f1f1d2cee40e51ebe9256273a156fff0
meiivis <ml8826@nyu.edu>
2025-12-01 02:58:06 +0100

    JWT with mangodb

--- back-end/config/jwt-config.js
+++ back-end/config/jwt-config.js
@@ -1,33 +1,33 @@
 const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt")
+const mongoose = require("mongoose")
 const User = require("../models/User")
 
-const jwtSecret = process.env.JWT_SECRET || "dev_local_jwt_secret"
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("jwt"), // Authorization: JWT <token>
+  secretOrKey: process.env.JWT_SECRET, // must be set in .env
+}
 
-const configureJwtStrategy = (passport) => {
-  const options = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("jwt"), // expect "Authorization: JWT <token>"
-    secretOrKey: jwtSecret,
+const jwtVerifyToken = async (jwtPayload, done) => {
+  // extra expiry guard (passport-jwt also checks exp)
+  if (jwtPayload.exp && new Date(jwtPayload.exp * 1000) < new Date()) {
+    return done(null, false, { message: "JWT token has expired." })
   }
 
-  passport.use(
-    new JwtStrategy(options, async (jwtPayload, done) => {
-      try {
-        // extra expiry guard (passport-jwt also checks exp)
-        if (jwtPayload.exp && new Date(jwtPayload.exp * 1000) < new Date()) {
-          return done(null, false, { message: "JWT token has expired." })
-        }
+  try {
+    const userId = new mongoose.Types.ObjectId(jwtPayload.id)
+    const user = await User.findOne({ _id: userId }).select("username")
+    if (!user) {
+      return done(null, false, { message: "User not found" })
+    }
 
-        const user = await User.findById(jwtPayload.id).select("username")
-        if (!user) {
-          return done(null, false, { message: "User not found" })
-        }
+    return done(null, user)
+  } catch (err) {
+    return done(err, false)
+  }
+}
 
-        return done(null, { id: user.id, username: user.username })
-      } catch (err) {
-        return done(err, false)
-      }
-    })
-  )
+const configureJwtStrategy = (passport) => {
+  passport.use(new JwtStrategy(jwtOptions, jwtVerifyToken))
 }
 
 module.exports = configureJwtStrategy
